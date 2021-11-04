392529c91a20c8e93cd1cd5a654c3756570d0b2b
LBU <vvookly@gmail.com>
2021-11-03 20:32:50 -0400

    stack vps

--- Stack/arrayStack.py
+++ Stack/arrayStack.py
@@ -1,39 +1,60 @@
-class ArrayStack:
-    def __init__(self):
-        self.data = []  # 빈 스택을 초기화
+# class ArrayStack:
+#     def __init__(self):
+#         self.data = []  # 빈 스택을 초기화
 
-    def size(self):
-        return len(self.data)  # 스택의 크기를 리턴
+#     def size(self):
+#         return len(self.data)  # 스택의 크기를 리턴
 
-    def isEmpty(self):
-        return self.size() == 0  # 스택이 비어 있는지 판단
+#     def isEmpty(self):
+#         return self.size() == 0  # 스택이 비어 있는지 판단
 
-    def push(self, item):
-        self.data.append(item)
+#     def push(self, item):
+#         self.data.append(item)
 
-    def pop(self):
-        return self.data.pop()
+#     def pop(self):
+#         return self.data.pop()
 
-    def peek(self):
-        return self.data[-1]
+#     def peek(self):
+#         return self.data[-1]
 
 
-class LinkedListStack:
-    def __init__(self):
-        self.data = DoublyLinkedList()
+# class LinkedListStack:
+#     def __init__(self):
+#         self.data = DoublyLinkedList()
 
-    def size(self):
-        return self.data.getLength()
+#     def size(self):
+#         return self.data.getLength()
 
-    def isEmpty(self):
-        return self.size() == 0
+#     def isEmpty(self):
+#         return self.size() == 0
 
-    def push(self, item):
-        node = Node(item)
-        self.data.insertAt(self.size() + 1, node)
+#     def push(self, item):
+#         node = Node(item)
+#         self.data.insertAt(self.size() + 1, node)
 
-    def pop(self):
-        return self.data.popAt(self.size())
+#     def pop(self):
+#         return self.data.popAt(self.size())
 
-    def peek(self):
-        return self.data.getAt(self.size()).data
+#     def peek(self):
+#         return self.data.getAt(self.size()).data
+
+
+def solution(expr):
+    match = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    S = ArrayStack()
+    for c in expr:
+        if c in '({[':
+            S.push(c)
+        elif c in match:
+            if S.size() == 0:
+                return False
+            else:
+                t = match[c]
+                if t != S.pop():
+                    return False
+    return S.isEmpty()
+#
