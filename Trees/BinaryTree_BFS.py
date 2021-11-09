9840dac9a8ad2a3096fd7eff4b2c5d69e369c02c
LBU <vvookly@gmail.com>
2021-11-08 22:56:43 -0500

    tree bfs

--- Trees/BinaryTree_BFS.py
+++ Trees/BinaryTree_BFS.py
@@ -75,3 +75,19 @@ class BinaryTree:
             return self.root.postorder()
         else:
             return []
+
+    def bfs(self):
+        traversal = []
+        Q = ArrayQueue()
+        if self.root == None:
+            return traversal
+        else:
+            Q.enqueue(self.root)
+        while not Q.isEmpty():
+            visit = Q.dequeue()
+            traversal.append(visit.data)
+            if visit.left:
+                Q.enqueue(visit.left)
+            if visit.right:
+                Q.enqueue(visit.right)
+        return traversal
