1d1e92118a4095ccfea182a35e4be547712bf4f7
WOOK <vvookly@gmail.com>
2022-03-20 01:01:25 -0400

    leetcode53

---
+++ neetcode/easy/leetcode53.py
@@ -0,0 +1,16 @@
+def maxSubArray(nums):
+    maxSub = nums[0]
+    curSum = 0
+
+    for i in nums:
+        if curSum < 0:
+            curSum = i
+        else:
+            curSum += i
+
+        maxSub = max(maxSub, curSum)
+    return maxSub
+
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(maxSubArray(nums))
